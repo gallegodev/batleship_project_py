869eb05a90727d5cf7d6ff959e17af0013ef272d
gallegodev <gallegodev1991@gmail.com>
2023-01-26 18:45:15 +0000

    Add ai_board function for better access and guidance of the code

--- run.py
+++ run.py
@@ -31,13 +31,35 @@ def user_board(hit,miss,complete):
 hit = hit_list
 miss = miss_list
 complete = ship_position_list
+
 user_board(hit,miss,complete)
 
 print("\n")
 
 # AI Board
 
-print( "Evil Pirate Board  \n")
-print("   0   1   2   3   4   5   6   7   8   9")
-for x in range(10):
-    print(x," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ ")
+def ai_board(hit,miss,complete):
+    print("Evil Pirate  Board\n")
+    print("    0  1  2  3  4  5  6  7  8  9")
+
+    place = 0
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            position = " _ "
+            if place in miss:
+                position = " x "
+            elif place in hit:
+                position = " o "
+            elif place in complete:
+                position = " S "
+        
+            row = row + position
+            place = place + 1
+        print(x, "", row)
+
+hit = hit_list
+miss = miss_list
+complete = ship_position_list
+
+ai_board(hit,miss,complete)
