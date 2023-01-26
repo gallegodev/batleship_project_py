05e1615fbfccaaed62a9b3aebe21882a4965df3d
gallegodev <gallegodev1991@gmail.com>
2023-01-26 21:50:55 +0000

    Add second boat to the board and once the player hit the 2 boats the will stop and the frasewith a winner will be shown

--- run.py
+++ run.py
@@ -7,27 +7,9 @@ ship_position_list = random.sample(range(0, 99), 2)
 
 player = input("Whats your name Commander?  \n")
 
-
-def player_shot(guesses):
-    attempt = "n"
-    while attempt == "n":
-        try:
-            shot = input(f"Commander {player} please enter your guess:\n")
-            shot = int(shot)
-            if shot < 0 or shot > 99:
-                print("PLease Commander enter a number between 0 and 99")
-            elif shot in guesses:
-                print("Commander you already picked this number, try anotehr")
-            else:
-                attempt = "y"
-                break 
-        except:
-            print("Incorrect input, please Commander insert only numbers")
-        
-    return shot
-
-
 # Player board
+
+
 def user_board(hit, miss, complete):
     print("              BATLESHIP!!!              \n")
     print(f"Commander {player} Board\n")
@@ -50,7 +32,26 @@ def user_board(hit, miss, complete):
         print(x, "", row)
 
 
-def check_shot(shot, hit, miss, complete, boat1):
+def player_shot(guesses):
+    attempt = "n"
+    while attempt == "n":
+        try:
+            shot = input(f"Commander {player} please enter your guess:\n")
+            shot = int(shot)
+            if shot < 0 or shot > 99:
+                print("PLease Commander enter a number between 0 and 99")
+            elif shot in guesses:
+                print("Commander you already picked this number, try anotehr")
+            else:
+                attempt = "y"
+                break 
+        except:
+            print("Incorrect input, please Commander insert only numbers")
+        
+    return shot
+
+
+def check_shot(shot,  boat1, boat2, miss, hit, complete):
     if shot in boat1:
         boat1.remove(shot)
         if len(boat1) > 0:
@@ -59,10 +60,21 @@ def check_shot(shot, hit, miss, complete, boat1):
             complete.append(shot)
     else:
         miss.append(shot)
-    return boat1, miss, hit, complete
+
+    if shot in boat2:
+        boat2.remove(shot)
+        if len(boat2) > 0:
+            hit.append(shot)
+        else:
+            complete.append(shot)
+    else:
+        miss.append(shot) 
+
+    return boat1, boat2, miss, hit, complete
 
 
 boat1 = [44, 45, 46]
+boat2 = [16, 17, 18]
 hit = []
 miss = []
 complete = []
@@ -70,13 +82,18 @@ complete = []
 for i in range(10):
     guesses = miss + hit + complete
     shot = player_shot(guesses)
-    boat1, hit, miss, complete = check_shot(shot, hit, miss, complete, boat1)
+    boat1, boat2, hit, miss, complete = check_shot(shot, boat1, boat2, hit, miss, complete)
     user_board(hit, miss, complete)
+
+    if len(boat1) < 1 and len(boat2) < 1:
+        print(f"Commander {player} you destroy the ships, YOU WON!!!!\n")
+        break
 
 print("\n")
 
 # AI Board
 # Need to fix random numbers for AI board / they are the same as the player
+
 
 def ai_board(hit, miss, complete):
     print("Evil Pirate  Board\n")
@@ -97,6 +114,7 @@ def ai_board(hit, miss, complete):
             row = row + position
             place = place + 1
         print(x, "", row)
+
 
 hit = hit_list
 miss = miss_list
