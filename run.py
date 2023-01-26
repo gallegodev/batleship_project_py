68b5c0f19b86a72359a4ad65ce461de9d90194b1
gallegodev <gallegodev1991@gmail.com>
2023-01-26 15:37:34 +0000

    Creating the board of the game

--- run.py
+++ run.py
@@ -1,6 +1,2 @@
-number = int(input("Please enter a number: "))
-
-if number == 5:
-    print(f"{number} is equal to 5")
-else:
-    print(f"{number} is not equal to 5")
+for x in range(10):
+    print(x," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ ")
