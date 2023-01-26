e523756a6c5581792c6403fca3e9bc850d347d7d
gallegodev <gallegodev1991@gmail.com>
2023-01-26 16:10:18 +0000

    Add notes for better guidance of the code

--- run.py
+++ run.py
@@ -1,11 +1,19 @@
+#Name Input for the player
+
 player = input("Whats your name Commander?  \n")
+
+#Player board
 
 print("              BATLESHIP!!!              \n")
 print(       f"Commander {player} Board         \n")
 print("   0   1   2   3   4   5   6   7   8   9")
 for x in range(10):
     print(x," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ ")
+
 print("\n")
+
+#AI Board
+
 print( "Evil Pirate Board  \n")
 print("   0   1   2   3   4   5   6   7   8   9")
 for x in range(10):
