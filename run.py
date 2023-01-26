657739571b0929e0b1336fa3ab2ffcc0828acb64
gallegodev <gallegodev1991@gmail.com>
2023-01-26 15:52:57 +0000

    Add player board and input for the player name

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+player = input("Whats your name Commander?  \n")
+
+print("              BATLESHIP!!!              \n")
+print(       f"Commander {player} Board         \n")
 print("   0   1   2   3   4   5   6   7   8   9")
 for x in range(10):
     print(x," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ ")
