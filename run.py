b971b348c8f602e275641f6dbbc68e3ee622958e
gallegodev <gallegodev1991@gmail.com>
2023-01-26 15:13:48 +0000

    First commit and test to check if python3 command is working

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+number = int(input("Please enter a number: "))
+
+if number == 5:
+    print(f"{number} is equal to 5")
+else:
+    print(f"{number} is not equal to 5")
