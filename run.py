9b995e78eccbf281c8798f1e0ea98982897b15d2
gallegodev <gallegodev1991@gmail.com>
2023-01-26 18:40:16 +0000

    Add function user_board for easier access to the code

--- run.py
+++ run.py
@@ -7,30 +7,31 @@ ship_position_list = random.sample(range(0, 99), 2)
 player = input("Whats your name Commander?  \n")
 
 # Player board
+def user_board(hit,miss,complete):
+    print("              BATLESHIP!!!              \n")
+    print(f"Commander {player} Board\n")
+    print("    0  1  2  3  4  5  6  7  8  9")
 
-print("              BATLESHIP!!!              \n")
-print(       f"Commander {player} Board         \n")
-print("    0  1  2  3  4  5  6  7  8  9")
+    place = 0
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            position = " _ "
+            if place in miss:
+                position = " x "
+            elif place in hit:
+                position = " o "
+            elif place in complete:
+                position = " S "
+        
+            row = row + position
+            place = place + 1
+        print(x, "", row)
 
 hit = hit_list
 miss = miss_list
 complete = ship_position_list
-place = 0
-
-for x in range(10):
-    row = ""
-    for y in range(10):
-        position = " _ "
-        if place in miss:
-            position = " x "
-        elif place in hit:
-            position = " o "
-        elif place in complete:
-            position = " S "
-        
-        row = row + position
-        place = place + 1
-    print(x, "", row)
+user_board(hit,miss,complete)
 
 print("\n")
 
