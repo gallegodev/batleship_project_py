c0d6c394d31ab3baeaf022df598a17cf86f908ca
gallegodev <gallegodev1991@gmail.com>
2023-01-26 21:19:59 +0000

    removed ai_board and fix mistakes around the code according to problems described on problems terminal

--- run.py
+++ run.py
@@ -8,27 +8,27 @@ ship_position_list = random.sample(range(0, 99), 2)
 player = input("Whats your name Commander?  \n")
 
 
-def player_shot():
-    attempt = True
-    while attempt == True:
+def player_shot(guesses):
+    attempt = "n"
+    while attempt == "n":
         try:
             shot = input(f"Commander {player} please enter your guess:\n")
             shot = int(shot)
             if shot < 0 or shot > 99:
                 print("PLease Commander enter a number between 0 and 99")
             elif shot in guesses:
-                print("Commander you already picked this number, please pick another one")
+                print("Commander you already picked this number, try anotehr")
             else:
-                attempt = False
+                attempt = "y"
                 break 
         except:
-            print("Incorrect input, please Commander insert a number between 0 to 99\n")
+            print("Incorrect input, please Commander insert only numbers")
         
     return shot
 
 
 # Player board
-def user_board(hit,miss,complete):
+def user_board(hit, miss, complete):
     print("              BATLESHIP!!!              \n")
     print(f"Commander {player} Board\n")
     print("    0  1  2  3  4  5  6  7  8  9")
@@ -49,6 +49,7 @@ def user_board(hit,miss,complete):
             place = place + 1
         print(x, "", row)
 
+
 def check_shot(shot, hit, miss, complete, boat1):
     if shot in boat1:
         boat1.remove(shot)
@@ -61,43 +62,16 @@ def check_shot(shot, hit, miss, complete, boat1):
     return boat1, miss, hit, complete
 
 
-boat1 = [45,46,47]
+boat1 = [44, 45, 46]
 hit = []
 miss = []
 complete = []
 
-guesses = miss + hit + complet
-shot = player_shot(guesses)
-boat1, hit, miss, complete = check_shot(shot, hit, miss, complete, boat1)
-user_board(hit,miss,complete)
+for i in range(10):
+    guesses = miss + hit + complete
+    shot = player_shot(guesses)
+    boat1, hit, miss, complete = check_shot(shot, hit, miss, complete, boat1)
+    user_board(hit, miss, complete)
 
 print("\n")
 
-# AI Board
-# Need to fix random numbers for AI board / they are the same as the player
-
-def ai_board(hit,miss,complete):
-    print("Evil Pirate  Board\n")
-    print("    0  1  2  3  4  5  6  7  8  9")
-
-    place = 0
-    for x in range(10):
-        row = ""
-        for y in range(10):
-            position = " _ "
-            if place in miss:
-                position = " x "
-            elif place in hit:
-                position = " o "
-            elif place in complete:
-                position = " S "
-        
-            row = row + position
-            place = place + 1
-        print(x, "", row)
-
-hit = hit_list
-miss = miss_list
-complete = ship_position_list
-
-ai_board(hit,miss,complete)
