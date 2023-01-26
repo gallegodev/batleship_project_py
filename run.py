b23e07d93ca83d4f265c34eb0219747d92c47c0e
gallegodev <gallegodev1991@gmail.com>
2023-01-26 16:06:20 +0000

    Add computer board to play against the player

--- run.py
+++ run.py
@@ -5,3 +5,8 @@ print(       f"Commander {player} Board         \n")
 print("   0   1   2   3   4   5   6   7   8   9")
 for x in range(10):
     print(x," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ ")
+print("\n")
+print( "Evil Pirate Board  \n")
+print("   0   1   2   3   4   5   6   7   8   9")
+for x in range(10):
+    print(x," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ ")
