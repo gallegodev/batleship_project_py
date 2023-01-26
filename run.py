fbbb45ece42c2b57b0e31feb468347795e863c26
gallegodev <gallegodev1991@gmail.com>
2023-01-26 16:36:36 +0000

    Add hit concept to the player board and change structure of the player board

--- run.py
+++ run.py
@@ -1,18 +1,28 @@
-#Name Input for the player
+# Name Input for the player
 
 player = input("Whats your name Commander?  \n")
 
-#Player board
+# Player board
 
 print("              BATLESHIP!!!              \n")
 print(       f"Commander {player} Board         \n")
-print("   0   1   2   3   4   5   6   7   8   9")
+print("    0  1  2  3  4  5  6  7  8  9")
+
+hit = [21,22,23]
+place = 0
 for x in range(10):
-    print(x," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ "," _ ")
+    row = ""
+    for y in range(10):
+        position = " _ "
+        if place in hit:
+            position = " x "
+        row = row + position
+        place = place + 1
+    print(x, "", row)
 
 print("\n")
 
-#AI Board
+# AI Board
 
 print( "Evil Pirate Board  \n")
 print("   0   1   2   3   4   5   6   7   8   9")
